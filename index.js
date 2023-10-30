1a8c121b8c2f8551b70cf3847933423dcc0be1eb
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-10-30 19:02:46 +0100

    working reg

---
+++ index.js
@@ -0,0 +1,149 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDqVxDsnFHOIRIRUySZW3yngUCoSaHbl3w",
+  authDomain: "loginpage-5820f.firebaseapp.com",
+  databaseURL:
+    "https://loginpage-5820f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "loginpage-5820f",
+  storageBucket: "loginpage-5820f.appspot.com",
+  messagingSenderId: "465705589202",
+  appId: "1:465705589202:web:5c17d45c9122945889d00b",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
+
+// Set up our register function
+function register() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+  full_name = document.getElementById("full_name").value;
+  favourite_song = document.getElementById("favourite_song").value;
+  milk_before_cereal = document.getElementById("milk_before_cereal").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+  }
+  if (
+    validate_field(full_name) == false ||
+    validate_field(favourite_song) == false ||
+    validate_field(milk_before_cereal) == false
+  ) {
+    alert("One or More Extra Fields is Outta Line!!");
+    return;
+  }
+
+  // Move on with Auth
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(function (userCredential) {
+      // Declare user variable
+      var user = userCredential.user;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        email: email,
+        full_name: full_name,
+        favourite_song: favourite_song,
+        milk_before_cereal: milk_before_cereal,
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).set(user_data);
+
+      // Done
+      alert("User Created!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Set up our login function
+function login() {
+  // Get all our input fields
+  email = document.getElementById("email").value;
+  password = document.getElementById("password").value;
+
+  // Validate input fields
+  if (validate_email(email) == false || validate_password(password) == false) {
+    alert("Email or Password is Outta Line!!");
+    return;
+    // Don't continue running the code
+  }
+
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then(function () {
+      // Declare user variable
+      var user = auth.currentUser;
+
+      // Add this user to Firebase Database
+      var database_ref = database.ref();
+
+      // Create User data
+      var user_data = {
+        last_login: Date.now(),
+      };
+
+      // Push to Firebase Database
+      database_ref.child("users/" + user.uid).update(user_data);
+
+      // DOne
+      alert("User Logged In!!");
+    })
+    .catch(function (error) {
+      // Firebase will use this to alert of its errors
+      var error_code = error.code;
+      var error_message = error.message;
+
+      alert(error_message);
+    });
+}
+
+// Validate Functions
+function validate_email(email) {
+  expression = /^[^@]+@\w+(\.\w+)+\w$/;
+  if (expression.test(email) == true) {
+    // Email is good
+    return true;
+  } else {
+    // Email is not good
+    return false;
+  }
+}
+
+function validate_password(password) {
+  // Firebase only accepts lengths greater than 6
+  if (password < 6) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validate_field(field) {
+  if (field == null) {
+    return false;
+  }
+
+  if (field.length <= 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
